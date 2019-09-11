1b462bd63ab68fba12dee9b56954cfa9f7208773
chelsea148629 <chelsea148629@gmail.com>
2019-09-11 22:04:00 +0800

    TestEnum

--- src/main/java/TestEnum.java
+++ src/main/java/TestEnum.java
@@ -1,12 +1,12 @@
 enum TestGreeting
 {
-    HI, Hello;
+    AAA, BBB;
 }
 
 public class TestEnum {
     public static TestGreeting greet()
     {
-        TestGreeting c1 = TestGreeting.HI;
+        TestGreeting c1 = TestGreeting.AAA;
         return c1;
     }
 }
